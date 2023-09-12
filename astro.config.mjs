28f6e84688d722cb40d41ae0270de6b80f4c03c1
DeKok, Matt G <MGD3@na.valmont.com>
2023-09-12 11:15:34 -0500

    Switch to node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import mdx from "@astrojs/mdx";
+import nodejs from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "hybrid",
-	adapter: vercel(),
+	adapter: nodejs({
+		mode: "middleware"
+	}),
 	integrations: [tailwind(), mdx()],
 	markdown: {
 		syntaxHighlight: "prism",
